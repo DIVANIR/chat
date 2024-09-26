caf1aad766ca9a5b3505d54b8e3fe2b55873f2db
Divanir de Jesus Silva <50552000+DIVANIR@users.noreply.github.com>
2024-09-25 21:09:54 -0300

    Update server.js

--- server.js
+++ server.js
@@ -68,7 +68,7 @@ io.on('connection', (socket) => {
 			data.msg.blob.url = `/file/${id}`
 		}
 		//io.emit('chat message', data)
-		io.emit('chat message',{{msg:{text:'chat desativado'}})
+		io.emit('chat message',{msg:{text:'chat desativado'}})
 		console.log(data.msg.text)
 		messages.push(data)
 		users.forEach((user) => {
